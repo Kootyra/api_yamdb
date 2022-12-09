28a6ff1a431d50931a1b25eb89abc83b8380c482
Andrey <andreykatkov13@gmail.com>
2022-12-09 04:21:35 +0300

    Revert "Feature/reviews comments"

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,12 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
-app_name = 'api'
-
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'genres', GenreViewSet, basename='genre')
 router.register(r'titles', TitleViewSet, basename='title')
+
 
 urlpatterns = [
     path('', include(router.urls))
