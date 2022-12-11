1e79b6617b266951fc7d2e05cc1419e1ad0cf361
Kootyra <110038102+Kootyra@users.noreply.github.com>
2022-12-11 10:04:50 +0100

    finish

--- api_yamdb/api/const.py
+++
@@ -1,2 +0,0 @@
-EMAIL_SENDER = 'from@example.com'
-CHARS_IN_RETURN = 100
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,6 +1,9 @@
 import os
 from datetime import timedelta
 
+EMAIL_SENDER = 'from@example.com'
+CHARS_IN_RETURN = 100
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
